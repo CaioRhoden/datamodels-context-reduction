e27337c334b5f4a6b4b529a3ba38941a1abe08a8
CaioRhoden <caiorhoden@gmail.com>
2024-11-08 11:24:53 -0300

    datamodels comparison fixes to new DatamodelsPipeline config

--- experiments/datamodels-retriever-comparison/create_datamodels_collection.py
+++ experiments/datamodels-retriever-comparison/create_datamodels_collection.py
@@ -1,7 +1,5 @@
 
-from src.utils import split_dev_set, subset_df
-from src.retriever import DatamodelsRetriever
-from src.datamodels_pipeline import Datamodels, DatamodelConfig
+from src.datamodels_pipeline import DatamodelPipeline, DatamodelConfig
 from src.llms import Llama3_1
 from src.evaluator import Rouge_L_evaluator
 import pandas as pd
@@ -12,40 +10,34 @@ def run_collection():
     
     llama = Llama3_1()
 
+
     config = DatamodelConfig(
         k = 8,
-        train_collections_idx_path = "../../data/instruction-induction-data/datamodels_15_10_2024/train_collection.h5",
+        num_models= 315,
+        datamodels_path = "../../data/instruction-induction-data/datamodels/datamodels_06_11_2024",
         train_collections_idx = None,
-        test_collections_idx_path = "../../data/instruction-induction-data/datamodels_15_10_2024/test_collection.h5",
         test_collections_idx = None,
         test_set = None,
-        test_set_path = "../../data/instruction-induction-data/datamodels_15_10_2024/dev_set.csv",
         train_set = None,
-        train_set_path = "../../data/instruction-induction-data/datamodels_15_10_2024/train_set.csv",
-        collections_path = "../../data/instruction-induction-data/datamodels_15_10_2024/collections/15-10-2024",
-        pre_collections_path = "../../data/instruction-induction-data/datamodels_15_10_2024/pre_collections/15-10-2024",
         instructions= None,
-        instructions_path= "../../data/instruction-induction-data/datamodels_15_10_2024/intructions.json",
         llm = llama,
-        model =  None,
-        evaluator= Rouge_L_evaluator(),
+        evaluator=Rouge_L_evaluator(),
     )
 
-
-    datamodel = Datamodels(config)
-    datamodel.set_test_set()
-    datamodel.set_train_set()
-    datamodel.set_train_collection_index()
-    datamodel.set_test_collection_index()
+    datamodel = DatamodelPipeline(config)
+    datamodel.set_collections_index()
+    datamodel.set_dataframes()
     datamodel.set_instructions_from_path()
 
 
 
     # Specify the folder path
-    folder_path = '../../data/instruction-induction-data/datamodels_15_10_2024/pre_collections/15-10-2024/'
+    folder_path = '../../data/instruction-induction-data/datamodels/datamodels_06_11_2024/pre_collections/'
 
     # Get a list of all files in the folder
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    files.sort()
+    
     for i in range(0, len(files)):
         print(f"Creating collection for {files[i]}")
         df = pd.read_pickle(folder_path + files[i])
@@ -53,6 +45,8 @@ def run_collection():
         datamodel.create_collection(batch_name=f_name, pre_collection_batch=df)
         print(f_name)
         print(df.shape)
+
+    
 
 
 if __name__ == "__main__":
