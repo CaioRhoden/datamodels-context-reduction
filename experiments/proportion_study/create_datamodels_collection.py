730413a5f015554ab0dc445a553a85b39771397a
CaioRhoden <caiorhoden@gmail.com>
2024-12-18 18:20:46 +0000

    change in collection creation to use the GPT2

--- experiments/proportion_study/create_datamodels_collection.py
+++ experiments/proportion_study/create_datamodels_collection.py
@@ -1,7 +1,7 @@
 
 from src.datamodels.pipeline import DatamodelPipeline
 from src.datamodels.config import DatamodelConfig
-from src.llms import Llama3_1
+from src.llms import GPT2
 from src.evaluator import GleuEvaluator
 import argparse
 import pandas as pd
@@ -11,13 +11,17 @@ import os
 def extract_idx(file: str):
     return int(file.split("_")[-1].split(".")[0])
 
+def format_collection_filename(filename: str):
+    name = os.path.splitext(os.path.basename(filename))[0].replace("pre_", "")
+    return name
+
 def run_collection(start_idx, end_idx, test_flag = False):
-
+    
 
     
     
-    llama = Llama3_1()
-    data_dir =  "../../data/instruction-induction-data/datamodels/proportion_study/210_5"
+    llama = GPT2()
+    data_dir =  "../../data/instruction-induction-data/datamodels/proportion_study/gpt2_420_5"
 
 
     config = DatamodelConfig(
@@ -47,6 +51,7 @@ def run_collection(start_idx, end_idx, test_flag = False):
     # Get a list of all files in the folder
     files = [f for f in os.listdir(pre_collection_path) if os.path.isfile(os.path.join(pre_collection_path, f))]
     collections = [f for f in os.listdir(collection_path) if os.path.isfile(os.path.join(collection_path, f))]
+    print(len(files))
     
     test_files = [f for f in files if f.startswith("test")]
     train_files = [f for f in files if not f.startswith("test")]
@@ -55,19 +60,22 @@ def run_collection(start_idx, end_idx, test_flag = False):
         ordered_files = sorted(test_files, key=extract_idx)
     else:
         ordered_files = sorted(train_files, key=extract_idx)
-        print(ordered_files[0])
+
+    ## Select the non repeated collections
+    f_names = [format_collection_filename(ordered_files[i]) for i in range(len(ordered_files))]
+    f_names = [name for name in f_names if name not in collections]
+    end_idx = min(len(f_names), end_idx)
 
     if end_idx == -1:
         end_idx = len(ordered_files)
     
+
+    
     for i in range(start_idx, end_idx):
-        print(f"Creating collection from pre_collection {i}")
-        print(datetime.datetime.now())
-        df = pd.read_feather(pre_collection_path + ordered_files[i])
-        f_name = os.path.splitext(os.path.basename(ordered_files[i]))[0]
-        f_name_c = f_name.replace("pre_", "")
-        if f_name_c not in collections:
-            datamodel.create_collection(batch_name=f_name, pre_collection_batch=df)
+        print(f"Creating collection from pre_collection {i} of {end_idx}")
+
+        df = pd.read_feather(f"{pre_collection_path}pre_{f_names[i]}.feather")
+        datamodel.create_collection(batch_name=f"pre_{f_names[i]}", pre_collection_batch=df)
 
 
 if __name__ == "__main__":
