4996ddabbc1f9aafbaaa1c363863587882a7ef80
CaioRhoden <caiorhoden@gmail.com>
2025-01-04 05:06:33 -0300

    abstract base module class for Datamodels

---
+++ src/datamodels/modules/BaseModule.py
@@ -0,0 +1,39 @@
+import ABC, abstractmethod
+from src.datamodels.modules.ModuleConfig import ModuleConfig
+
+class BaseModule(ABC):
+    @abstractmethod
+    def __init__(self, module_config: ModuleConfig):
+        """
+        Initializes a new instance of the BaseModule class to be used on the Datamodels pipeline.
+
+        Parameters:
+            module_config(ModuleConfig): The configuration for the Datamodels module.
+        """
+        pass
+    
+
+    @abstractmethod
+    def __str__(self):
+        """
+        Returns a string representation of the handler.
+
+        Returns:
+            str: A string representation of the handler.
+        """
+        pass
+
+    @abstractmethod
+    def run(self):
+        """
+        Executes the main functionality of the handler.
+
+        This method needs to be implemented by subclasses to define
+        the specific behavior of the handler.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a subclass.
+        """
+
+        pass
+
