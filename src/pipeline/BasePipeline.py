fd418b0e35a371bf86d277400baaca4830720516
CaioRhoden <caiorhoden@gmail.com>
2025-01-07 17:34:41 -0300

    create pipeline class to run instructions baselines

--- src/pipeline/BasePipeline.py
+++ src/pipeline/BasePipeline.py
@@ -1,109 +1,23 @@
-from src.dataloader.BaseDataloader import BaseDataloader
-# from src.vector_store import BaseVectorStore
-# from langchain.embeddings import HuggingFaceBgeEmbeddings
-from src.llms import BaseLLM
-import torch
-from langchain.prompts import PromptTemplate
+
+
+from abc import ABC, abstractmethod
 import pandas as pd
 
 
-
-from src.retriever import BaseRetriever
-# from ..models import BaseModel
-
-
-
-
-class BasePipeline():
+class BasePipeline(ABC):
 
     
     def __init__(
                     self, 
-                    retriever: BaseRetriever,
-                    llm: BaseLLM,
-                    dataloader: BaseDataloader,
-                    device: str | None = "cpu",
                 ) -> None:
-        
-        torch.set_default_device(device)
-        self.device = device
-
-        self.dataloader = dataloader,
-        self.documents = dataloader.get_documents()
-
-        self.retriever = retriever
-        self.llm = llm
-    
+        pass
 
 
-
-
-
-        
-
-        # self.retriever = retriever,
-        # self.vector_store = vector_store,
-        # self.model = model
-
-
-
-
-
-    
+    @abstractmethod
     def run(self, input: str, k: int) -> str:
 
-        
-        retrieved_documents = self.retriever.retrieve(self.documents, k, input)
-        context = "\n".join([f"{doc.page_content} \n" for doc in retrieved_documents])
+        pass
 
-        template = """
-            Fill the expected Output
-        
-            Examples:
-            {context}
-
-            Input:
-            {input}
-
-            Output:
-        """
-        prompt = PromptTemplate.from_template(template).format(context=context, input=input)
-
-        
-
-
-        return self.llm.run(prompt)
-
-
-
-        
-    
-    def run_tests(self, test_data: pd.DataFrame, checkpoints_step: int, checkpoint: int, k: int, run_tag: str) -> None:
-
-        tasks, inputs, outputs, predicted = [], [], [], []
-
-        for i in range(checkpoint, len(test_data)):
-            print(f"Step {i} of {len(test_data)}")
-            tasks.append( test_data.loc[i]["task"])
-            inputs.append( test_data.loc[i]["input"])
-            outputs.append( test_data.loc[i]["output"])
-
-            input = str(test_data.loc[i]["input"])
-
-            predicted.append(self.run(input, k))
-
-            if i % checkpoints_step == 0 and i > checkpoint:
-
-                df = pd.DataFrame({"task": tasks, "input": inputs, "output": outputs, "predicted": predicted})
-                df.to_pickle(f"../../data/runs_id/{run_tag}/{i - checkpoints_step}_{i}.pickle")
-                tasks, inputs, outputs, predicted = [], [], [], []
-
-            elif i == len(test_data) - 1:
-         
-                df = pd.DataFrame({"task": tasks, "input": inputs, "output": outputs, "predicted": predicted})
-                df.to_pickle(f"../../data/runs_id/{run_tag}/{i - checkpoints_step}_{i}.pickle")
-
-
-
-
-
+    @abstractmethod
+    def run_tests(self, data: pd.DataFrame, checkpoints_step: int, checkpoint: int, k: int) -> None:
+       pass
--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,1 +1,3 @@
-from .BasePipeline import BasePipeline
+from src.pipeline.BasePipeline import BasePipeline
+from src.pipeline.BaselinePipeline import BaselinePipeline
+from src.pipeline.RAGPipeline import RAGPipeline
