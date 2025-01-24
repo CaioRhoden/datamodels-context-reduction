0aaa4986695c81cb43ca02042cbd877ae91ab951
CaioRhoden <caiorhoden@gmail.com>
2025-01-24 16:01:51 -0300

    rouge L evaluator fix

--- src/evaluator/Rouge_L_evaluator.py
+++ src/evaluator/Rouge_L_evaluator.py
@@ -10,29 +10,39 @@ class Rouge_L_evaluator(BaseEvaluator):
         self.rouge_l = evaluate.load("rouge")
 
     def evaluate(self, y: np.ndarray, y_pred: np.ndarray, possible_outputs: None | np.ndarray) -> np.ndarray:
-
-
         if y.shape != y_pred.shape:
             raise ValueError("The shape of predictions and references must be the same.")
         
         # Calculate Rouge-L for each pair of sentences
-        rouge_l_scores = []
-
+        results = []
         if possible_outputs is None:
             for pred, ref in zip(y_pred, y):
-                result = self.rouge_l.compute(predictions=[pred], references=[ref], use_stemmer=True)
-                rouge_l_scores.append(result["rougeL"])
-
+                result = self.rouge_l.compute(predictions=[pred], references=[ref])
+                if result is not None:
+                    results.append(result["rougeL"])
+                else:
+                    raise ValueError("Google BLEU cannot be computed.")
         else:
             for pred, ref, poss in zip(y_pred, y, possible_outputs):
-                result = self.rouge_l.compute(predictions=[pred], references=[ref], use_stemmer=True)
+                result = self.rouge_l.compute(predictions=[pred], references=[ref])
                 if type(poss) is not float:
                     poss = ast.literal_eval(poss)
-                    for i in range(len(poss)):
-                        candidate = self.rouge_l.compute(predictions=[pred], references=[poss[i]], use_stemmer=True)
-                        if candidate["rougeL"] > result["rougeL"]:
-                            result = candidate
-                rouge_l_scores.append(result["rougeL"])
 
+                    try:
+                        possible_preds = [self.rouge_l.compute(predictions=[pred], references=[p])["rougeL"] for p in poss] # type: ignore
+                    except:
+                        raise ValueError("Google BLEU cannot be computed for possible outputs")
+                    
+                    if result is not None:
+                         results.append(max(result["rougeL"],
+                                        *possible_preds))
+                    else:
+                        raise ValueError("Google BLEU cannot be computed.")
+                   
+                elif result is not None:
+                    results.append(result["rougeL"])
+
+                else:
+                    raise ValueError("Google BLEU cannot be computed.")
         
-        return np.array(rouge_l_scores)
+        return np.array(results)
