c5fad942774c4290cda4de05b7712630bdb6d63b
CaioRhoden <caiorhoden@gmail.com>
2025-03-07 12:22:41 +0000

    generic llm instruc using hf

--- src/llms/__init__.py
+++ src/llms/__init__.py
@@ -5,3 +5,4 @@ from src.llms.Llama3_1_Instruct import Llama3_1_Instruct
 from src.llms.GPT2 import GPT2
 from src.llms.GPT2Parser import GPT2Parser
 from src.llms.ParserLlama import ParserLlama
+from src.llms.GenericIntructModelHF import GenericIntructModelHF
