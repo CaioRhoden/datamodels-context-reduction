2169b03660a896c8886af2bd7c9aff42dc619053
CaioRhoden <caiorhoden@gmail.com>
2025-04-19 02:07:03 -0300

    test datamodel linear regression

---
+++ test/datamodels_pipelines/datamodels_nq_pipeline/test_nq_pipeline_train_datamodels.py
@@ -0,0 +1,77 @@
+import pytest
+import polars as pl
+import numpy as np
+from dmcr.datamodels.pipeline import DatamodelsNQPipeline
+from dmcr.datamodels import DatamodelConfig
+import torch
+
+from dmcr.utils.test_utils import clean_temp_folders
+import tempfile
+import os
+import shutil
+
+
+class TestNQPipelineCollectionCreation:
+
+    @classmethod
+    def setup_class(cls):
+
+        ## CREATE TEMP DATA FOR PIPELINE
+        clean_temp_folders()
+
+        tmp_path = tempfile.mkdtemp(dir=os.getcwd())
+        cls.datamodels_path = tmp_path
+
+        pre_collection = {
+            "collection_idx": [i for i in range(10)],
+            "test_idx": [0 for i in range(10)],
+            "input": [np.array([1,0,1]) for i in range(10)],
+            "evaluation": [0.5 for i in range(10)],
+        }
+
+        os.mkdir(f"{tmp_path}/collections")
+        os.mkdir(f"{tmp_path}/collections/train")
+        os.mkdir(f"{tmp_path}/collections/test")
+
+        df = pl.DataFrame(pre_collection)
+        df.write_ipc(f"{tmp_path}/collections/train/collections_test.feather")
+        df.write_ipc(f"{tmp_path}/collections/test/collections_test.feather")
+
+        config = DatamodelConfig(
+            k = 3,
+            num_models= 1,
+            datamodels_path = f"{tmp_path}",
+        )
+
+        pipeline = DatamodelsNQPipeline(config, test_flag=True)
+        pipeline.train_datamodels(
+            collection_name = "collections_test",
+            epochs=50,
+            train_batches=1,
+            val_batches=1,
+            val_size=0.2,
+            lr=0.001,
+            patience=10,
+            run_id="test",
+
+        )
+
+
+
+        
+        
+    @classmethod
+    def teardown_class(cls):
+        shutil.rmtree(cls.datamodels_path)
+        clean_temp_folders()
+
+    def test_output_generation_train_datamodels(self):
+        assert os.path.exists(f"{self.datamodels_path}/models/test")
+        assert os.path.exists(f"{self.datamodels_path}/models/test/weights.pt")
+        assert os.path.exists(f"{self.datamodels_path}/models/test/bias.pt")
+
+    def test_output_generation_train_datamodels(self):
+        weights = torch.load(f"{self.datamodels_path}/models/test/weights.pt", weights_only=True)
+        bias = torch.load(f"{self.datamodels_path}/models/test/bias.pt", weights_only=True)
+        assert weights.shape == (1, 3)
+        assert bias.shape == (1,)
